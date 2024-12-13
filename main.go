a630f8a9fa895082b1de98c360ae8a09d237d5ec
jasonwvh <jason.wongvh@gmail.com>
2024-12-13 15:18:39 +0800

    calculate with points

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	_ "github.com/lib/pq"
 )
@@ -18,113 +20,116 @@ const (
 	dbname   = "osm"
 )
 
-func main() {
+func initDb() (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	defer db.Close()
 
-	// alterTableSQL := `
-	// 	ALTER TABLE planet_osm_roads ADD COLUMN source integer;
-	// 	ALTER TABLE planet_osm_roads ADD COLUMN target integer;
-	// `
-	// _, err = db.Exec(alterTableSQL)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	return db, nil
+}
 
-	// initPgRoutingSQL := `
-	// 	SELECT pgr_createTopology('planet_osm_roads', 0.00001, 'way', 'osm_id');
-	// 	SELECT pgr_analyzeGraph('planet_osm_roads', 0.00001, 'way', 'osm_id');
-	// `
-	// _, err = db.Exec(initPgRoutingSQL)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+func initTopology(db *sql.DB) error {
+	alterTableSQL := `
+		ALTER TABLE planet_osm_roads ADD COLUMN source integer;
+		ALTER TABLE planet_osm_roads ADD COLUMN target integer;
+	`
+	_, err := db.Exec(alterTableSQL)
+	if err != nil {
+		return err
+	}
 
-	// fmt.Println("initialization complete")
+	initPgRoutingSQL := `
+		SELECT pgr_createTopology('planet_osm_roads', 0.00001, 'way', 'osm_id');
+		SELECT pgr_analyzeGraph('planet_osm_roads', 0.00001, 'way', 'osm_id');
+	`
+	_, err = db.Exec(initPgRoutingSQL)
+	if err != nil {
+		return err
+	}
 
-	//shortestPathSQL := `
-	//	SELECT r.osm_id, r.way, w.tags
-	//	FROM planet_osm_roads r
-	//	JOIN (SELECT * FROM pgr_dijkstra(
-	//	'SELECT osm_id AS id,
-	//		source,
-	//		target,
-	//		ST_Length(way) AS cost,
-	//		-ST_Length(way) AS reverse_cost
-	//		FROM planet_osm_roads',
-	//	104606, 110071,
-	//	directed := false)) sp
-	//	ON r.osm_id = sp.edge
-	//	JOIN planet_osm_ways w ON w.id = r.osm_id;`
+	fmt.Println("initialization complete")
+	return nil
+}
 
+func findCoords(db *sql.DB, id int) (Coordinate, error) {
 	findCoordSQL := `
 		SELECT
-		ST_X(ST_Transform (way, 3857)) AS longitude,
-		ST_Y(ST_Transform (way, 3857)) AS latitude
+		ST_X(ST_Transform(way, 3857)) AS longitude,
+		ST_Y(ST_Transform(way, 3857)) AS latitude
 		FROM planet_osm_point
-		WHERE osm_id = 2043147561
+		WHERE osm_id = $1
 	`
 
-	rows, err := db.Query(findCoordSQL)
+	rows, err := db.Query(findCoordSQL, id)
 	if err != nil {
-		log.Fatal(err)
+		return Coordinate{}, err
 	}
 	defer rows.Close()
 
 	var coord Coordinate
 	if rows.Next() {
 		if err := rows.Scan(&coord.Longitude, &coord.Latitude); err != nil {
-			log.Fatal(err)
+			return Coordinate{}, err
 		}
 	}
 
+	return coord, nil
+}
+
+func findRoads(db *sql.DB, coord Coordinate) (string, error) {
 	findRoadSQL := fmt.Sprintf(`
 		SELECT id FROM planet_osm_roads_vertices_pgr 
 		ORDER BY the_geom <-> (SELECT ST_SetSRID(ST_MakePoint(%f, %f), 3857)) 
 		LIMIT 1;
 	`, coord.Longitude, coord.Latitude)
 
-	rows, err = db.Query(findRoadSQL)
+	rows, err := db.Query(findRoadSQL)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	defer rows.Close()
 
 	var id string
 	if rows.Next() {
 		if err := rows.Scan(&id); err != nil {
-			log.Fatal(err)
+			return "", err
 		}
 	}
 
-	multinodeShortestPathSQL := `
-		SELECT r.osm_id, r.tags, ST_AsGeoJSON(r.way), r.source, sp.cost, sp.agg_cost
-		FROM planet_osm_roads r
-		JOIN (SELECT * FROM pgr_TSP(
-		  $$SELECT * FROM pgr_dijkstraCostMatrix(
-			'SELECT osm_id AS id, source, target, ST_Length(way) AS cost, -ST_Length(way) AS reverse_cost FROM planet_osm_roads',
-			(SELECT array_agg(id) FROM planet_osm_roads_vertices_pgr WHERE id IN (542, 524, 552)),
-			directed => false) $$)) sp
-		ON sp.node = r.source;
-	`
+	return id, nil
+}
 
-	rows, err = db.Query(multinodeShortestPathSQL)
+func findShortestPath(db *sql.DB, source string, target string) ([]Road, error) {
+	shortestPathSQL := fmt.Sprintf(`
+		SELECT r.osm_id, ST_AsGeoJSON(r.way), w.tags
+		FROM planet_osm_roads r
+		JOIN (SELECT * FROM pgr_dijkstra(
+		'SELECT osm_id AS id,
+			source,
+			target,
+			ST_Length(way) AS cost,
+			-ST_Length(way) AS reverse_cost
+			FROM planet_osm_roads',
+		%s, %s,
+		directed := false)) sp
+		ON r.osm_id = sp.edge
+		JOIN planet_osm_ways w ON w.id = r.osm_id;`, source, target)
+
+	rows, err := db.Query(shortestPathSQL)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer rows.Close()
 
 	var roads []Road
 	for rows.Next() {
 		var road Road
-		if err := rows.Scan(&road.OSMID, &road.Tags, &road.Way, &road.Source, &road.Cost, &road.AggCost); err != nil {
-			log.Fatal(err)
+		if err := rows.Scan(&road.OSMID, &road.Way, &road.Tags); err != nil {
+			return nil, err
 		}
 		roads = append(roads, road)
 	}
@@ -134,18 +139,122 @@ func main() {
 	}
 
 	if err := rows.Err(); err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	for _, road := range roads {
-		fmt.Printf("osm_id: %, ways: %s, tags: %s\n", road.OSMID, road.Way, road.Tags)
+	return roads, nil
+}
+
+func findBestRoute(db *sql.DB, nodeIDs []int) ([]Road, error) {
+	ids := make([]string, len(nodeIDs))
+	for i, id := range nodeIDs {
+		ids[i] = fmt.Sprintf("%d", id)
+	}
+	idsStr := strings.Join(ids, ",")
+
+	multinodeShortestPathSQL := fmt.Sprintf(`
+		SELECT r.osm_id, r.tags, ST_AsGeoJSON(r.way), r.source, sp.cost, sp.agg_cost
+		FROM planet_osm_roads r
+		JOIN (SELECT * FROM pgr_TSP(
+		  $$SELECT * FROM pgr_dijkstraCostMatrix(
+			'SELECT osm_id AS id, source, target, ST_Length(way) AS cost, -ST_Length(way) AS reverse_cost FROM planet_osm_roads',
+			(SELECT array_agg(id) FROM planet_osm_roads_vertices_pgr WHERE id IN (%s)),
+			directed => false) $$)) sp
+		ON sp.node = r.source;
+	`, idsStr)
+
+	rows, err := db.Query(multinodeShortestPathSQL)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var roads []Road
+	for rows.Next() {
+		var road Road
+		if err := rows.Scan(&road.OSMID, &road.Tags, &road.Way, &road.Source, &road.Cost, &road.AggCost); err != nil {
+			return nil, err
+		}
+		roads = append(roads, road)
+	}
+
+	if len(roads) > 0 {
+		roads = roads[:len(roads)-1] // Remove the last element
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return roads, nil
+}
+
+func main() {
+	db, err := initDb()
+	if err != nil {
+		return
 	}
 
 	tmpl := template.Must(template.New("map.gohtml").ParseFiles("map.gohtml"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := tmpl.Execute(w, struct{ Roads []Road }{roads})
+		osmIDs := r.URL.Query()["osm_id"]
+		if len(osmIDs) == 0 {
+			err = tmpl.Execute(w, struct{ Roads []Road }{})
+			return
+		}
+
+		var nodeIDs []int
+		for _, osmIDStr := range osmIDs {
+			osmID, err := strconv.Atoi(osmIDStr)
+			if err != nil {
+				http.Error(w, "Invalid osm_id parameter", http.StatusBadRequest)
+				return
+			}
+
+			coord, err := findCoords(db, osmID)
+			if err != nil {
+				http.Error(w, "Error finding coordinates", http.StatusInternalServerError)
+				return
+			}
+
+			id, err := findRoads(db, coord)
+			if err != nil {
+				http.Error(w, "Error finding roads", http.StatusInternalServerError)
+				return
+			}
+
+			nodeID, err := strconv.Atoi(id)
+			if err != nil {
+				http.Error(w, "Error converting road ID", http.StatusInternalServerError)
+				return
+			}
+
+			nodeIDs = append(nodeIDs, nodeID)
+		}
+
+		routes, err := findBestRoute(db, nodeIDs)
 		if err != nil {
-			log.Fatal(err)
+			http.Error(w, "Error finding shortest path", http.StatusInternalServerError)
+			return
+		}
+
+		var allRoads []Road
+		for i := 0; i < len(routes)-1; i++ {
+			source := routes[i].Source
+			target := routes[i+1].Source
+
+			roads, err := findShortestPath(db, source, target)
+			if err != nil {
+				return
+			}
+
+			allRoads = append(allRoads, roads...)
+		}
+
+		err = tmpl.Execute(w, struct{ Roads []Road }{allRoads})
+		if err != nil {
+			http.Error(w, "Error rendering template", http.StatusInternalServerError)
+			return
 		}
 	})
 	log.Println("Starting server on :9999")
